97996758454886eff1da021c307e0f8bc2b4260d
unknown <G00175895@W213-INSTR.academic.raritanval.edu>
2015-11-09 20:39:11 -0500

    Init Commit

---
+++ Creature.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Creature here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Creature
+{
+   private int hp;
+   private int strength;
+   
+   public Creature(){
+       
+   }
+    
+   public int damage(){
+       //TODO: change this
+       return 0;
+    }
+    
+}
